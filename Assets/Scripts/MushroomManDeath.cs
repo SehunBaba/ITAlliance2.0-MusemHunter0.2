e4dde0c61bdc51807e0862a3720777a5f18e4fda
Rik Ferge <37657214+rfearnley@users.noreply.github.com>
2018-04-10 01:15:14 +0100

    Initial commit

---
+++ Assets/Scripts/MushroomManDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomManDeath : MonoBehaviour {
+
+     public  GameObject MushroomMan;	
+	// Use this for initialization
+	void Start () {
+	}
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        this.GetComponent<Collider>().enabled=false;
+        GetComponent<MushroomManMove>().enabled = false;
+        MushroomMan.transform.localScale = new Vector3(0, 0, 0.5f);
+        MushroomMan.transform.localPosition = new Vector3(0, 0, 0.25f);
+        yield return new WaitForSeconds(0.495F);
+        MushroomMan.transform.localPosition = new Vector3 (0, -1000, 0);
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
